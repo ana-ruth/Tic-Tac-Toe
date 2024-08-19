2c998dc10fc52b952a28429cb66c3a396d6c480f
ana-ruth <114896716+ana-ruth@users.noreply.github.com>
2024-08-19 15:04:35 -0400

    Cell function for each square on board

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function Gameboard() {
+function Gameboard() 
+{
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -12,4 +13,39 @@ function Gameboard() {
 
     const getBoard = () => board;
 
+    //check cell to mark it
+    const markPlace = (row,column,player) => {
+
+        const selectedCell = board[row][column];
+
+        if(selectedCell != 0 ) return;
+    
+        const markedCell = selectedCell.addMark(player);
+    };
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell => cell.getValue())))
+        console.log(boardWithCellValues);
+    };
+
+    return { getBoard, markPlace, printBoard };
 }
+
+
+function Cell()
+{
+    let value = 0;
+
+    const addMark = (player) => {
+        value = player;
+    }
+    
+    const getValue = () => value;
+
+    return{
+        addMark,
+        getValue
+    };
+}
+
+
